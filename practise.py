be9a8eeda2fa28d9a824230183e1417b7a15797f
rohith <rohith@rohiths-MacBook-Air.local>
2025-10-23 20:11:09 +0530

    initial commit

---
+++ practise.py
@@ -0,0 +1,719 @@
+print("hello"); print("bye bye");print("see you") 
+
+
+
+
+print("Hello World!", end=" ")
+print('I will print on the same line.')
+
+
+
+
+print(3)
+print(358)
+print(50000)
+
+print(2+5)
+print(33*8)
+
+print("I am Rohith",   22, "years old.")
+
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x)
+print(y)
+print(z)
+
+
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+
+age = 19
+
+if age >=18:
+    print("your an adult.")
+
+elif 18==19:
+    print("You can child")
+
+else:
+    print("you are a minor")
+
+
+
+
+totallmarks = 365
+
+if totallmarks >=150:
+    print("pass")
+elif 365==100:
+    print("fail")
+else :
+       print("equal")
+
+
+
+  
+totallchairs = 75
+
+if totallchairs <=10:
+    print("missing")
+elif 75==10:
+    print("added")
+else:
+    print("totall 80")
+
+
+
+totallBuffaloes = 30
+
+if totallBuffaloes >=5:
+    print("")
+
+
+
+print("hello india")
+print("hello rohith")
+print("all  over indiA")
+
+print("india is great"); print("india is over");  print("all over india")
+
+print(354); print(408); print(510) 
+
+
+print("I am rohit",24,"years old")
+
+x=45
+y=45
+print(x,y)
+
+name="alice"
+age=23
+is_active=True
+print(name,age,is_active)
+
+
+
+Name="rohith"
+age=23
+gender="male"
+education="B.tech"
+city="vizag"
+print(Name,age,gender,education,city)
+
+
+
+Name,age,Gender,education,city,="rohith",23,"male","B.tech","vizag"
+print(Name,age,Gender,education,city)
+
+
+
+name=city=town="vizag"
+print(name)
+print(city)
+print(town)
+
+
+
+age=weight=height=22
+print(age)
+print(height)
+print(weight)
+
+
+
+
+# unpack list 
+
+person=["rohith",22,"Male",6.2]
+x,y,z,l= (person)
+print(x)
+print(l)
+
+
+
+
+Villagers=["people","gratitude","rashness","Doing Hard work"]
+a,b,c,d=(Villagers)
+print(a)
+print(d)
+print(b)
+print(c)
+
+
+
+# output varaiables
+
+print("my name is rohith i recently graduate from gitam univesrsity, 2025, year passed out")
+
+x=1000
+y="Rohith"
+hero=0
+print(x,y,hero)
+
+
+
+# Data types
+
+x=22
+print(type(x))
+
+x="rohith is a good boy"
+print(type(x))
+
+x=22.5
+print(type(x))
+
+x=True
+print(type(x))
+
+x=1j
+print(type(x))
+
+x=["apple","banana","cherry"]
+print(type(x))
+
+x=("apple","mango","watermelon")
+print(type(x))
+
+x={"apple","mango","watermelon"}
+print(type(x))
+
+name="rohith"
+age=23
+city="vizag"
+costofstudy=10.90
+print (type(name))
+
+
+
+# python strings
+
+print("he is the hero")
+print('he is good boy')
+
+
+R = "rohith Bonthu"
+print(R)
+
+a="Rohith is Graduated from gitam univesrsity"
+print(a)
+
+
+
+# Slicing Strings
+
+
+
+
+
+
+b= "Gitam University"
+print(b[3:4])
+
+
+c=("Gitam is the one of the top most university")
+print(c[5:8])
+
+
+k=("king is not beware person")
+print(k[3:9])
+
+
+
+# if we want to slice from ldmle2jojiorjeoi
+
+z=("Scorpio n")
+print(z[:3])
+
+h=("fiat punto")
+print(h[:6])
+
+# if we want to slice from ending
+
+f="we want to slice from ending"
+print(f[3:])
+
+# Modify strings
+
+r="Hello world"
+print(r.upper())
+
+r="Hello world"
+print(r.lower())
+
+a = "Rohith Bonthu"
+print(a.replace("B", "D"))
+
+
+a="king of universe"
+print(a.replace("g","D"))
+
+
+# Format strings
+
+cgpa= 78
+
+txt= f"pavan is topper of college, he is good  {cgpa}"
+print(txt)
+
+Lists in python 
+
+mylist =["apple","Mango","cherry","pineapple","apple", ]
+print(mylist)
+
+tuples
+
+mytuple = ("apple","Banana","cherry")
+print(mytuple)
+
+
+
+# duplicate tuple
+
+mytuple = ("rohith","Bhargav","pavan","avinash","Midhilesh","sivram","vamsi","sreenivas","Vaibhav","Bhanu","Bhargav")
+print(mytuple)
+
+mytuple = ("rohith","Bhargav","pavan","avinash","Midhilesh","sivram","vamsi","sreenivas","Vaibhav","Bhanu","Bhargav","mohanakhil","saketh","sridhar")
+print(len(mytuple))
+
+#sets
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+
+thisset = {"pavna","Rohith","Bhargav","avinash"}
+thisset.remove("pavna")
+print(thisset)
+
+
+thisset = {"green","yellow","orange"}
+thisset.add("hero")
+print(thisset)
+
+# Dictionaries
+
+student = {
+    "name": "rohith",
+    "age":  22,
+    "course":"cse",
+}
+print(student)
+
+
+scorpio = {
+  "gear":"Automatic",
+  "speed": 180,
+  "colour":"white",
+}
+print(scorpio)
+
+thisdict =	{
+  "brand": "ford",
+  "model": "ecosport",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "eco sport",
+  "year": 1964
+}
+x=thisdict["year"] = 2018
+print(x)
+
+thisdict = {
+    "name":"pavan",
+    "age":22,
+    "year":2002
+}
+x=thisdict["year"] = 2002
+print(x)
+
+thisdict = {
+
+    "school":"srichaitanya",
+    "students": 808,
+    "established":2018 
+}
+x=thisdict["students"] = 900
+print(x)
+
+# //adding list//#
+
+thisdict = {
+
+    "school":"srichaitanya",
+    "students": 808,
+    "established":2018 
+}
+thisdict["school buses"] = 47
+print(thisdict)
+
+thisdict = {
+    "college":"Gitam",
+    "Branch":"cse",
+    "Backlogs":2,   
+
+}
+thisdict["name"] = "sai"
+print(thisdict)
+
+
+# //pop means it  will remove elements
+thisdict = {
+ "college":"Gitam",
+ "Branch":"cse",
+ "Backlogs":2,   
+
+}
+thisdict.pop("college")
+print(thisdict)
+
+# #if else elif
+
+a = 380
+b = 470
+
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("b is greater than a")
+
+
+
+  #if practicing
+
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+
+rating = 5
+
+if rating >8:
+    print("hit")
+elif rating >=7:
+    print("super")
+elif rating >=6:
+    print("above average")
+else:
+    print("flop")
+
+age=18
+
+if age>18:
+   print ("you are minor")
+elif age==18:
+   print("you are elgibule to vote")
+else: 
+   print("you are not elgibule to vote")
+
+
+
+
+
+temprature=42
+
+if temprature>42:
+    print("its light hot")
+elif temprature>20:
+    print("weather is very cool")
+else:
+    print("weather is very moderate")
+   
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+
+
+
+
+name= "hello world"  #string
+a=3 #Int
+b=4.5 #Float
+c=2+3j #complex
+names = ["Avinash","Bhargav","pavan"]  #list
+fruits=("mango","Bananan","pineapple")  #tuple
+numbers = range(2, 8) #range
+
+
+# #string Integer
+name="rohith"
+age="twenty two"
+city="vizag"
+village="rangapuram"
+occupation="student"
+Btech="engineering"
+print(name,age,city,village,occupation,Btech)
+
+#int
+
+age=22
+speed=180
+weight=78
+cost=500000000
+print(age,speed,weight,cost)
+
+#Float
+height=6.2
+width =14.5
+length=15.5
+print(height,width,length)
+
+#complex
+
+a=2+1j
+b=3+2j
+l=5+6j
+print(a,b,l)
+
+#list
+my_list1 =["pavan","Bhargav","Rohith","Avinash","Sridhar"]
+# my_list2 =["sai","kumar","sreenivas","hero"]
+print(my_list1[1])
+
+
+
+#list
+my_list1 =["pavan","Bhargav","Rohith","Avinash","Sridhar"]
+# my_list2 =["sai","kumar","sreenivas","hero"]
+print(my_list1[0])
+
+list
+my_list1 =["pavan","Bhargav","Rohith","Avinash","Sridhar"]
+# my_list2 =["sai","kumar","sreenivas","hero"]
+print(my_list1[-3])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-2:5])
+
+thislist =["powerrangers","marvell","3dmovies"]
+print(thislist[:3])
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+your_list1=["scorpio","Bolero","fiat","Hycross"]
+your_list1[2]= "hondacity"
+print(your_list1)
+
+# my_list=["powerangers","motupatlu","gearup"]
+# my_list[3]="kingkong"
+# print(my_list)
+
+my_list=["powerangers","motupatlu","gearup"]
+my_list[1:2]="kingkong","mongodb"
+print(my_list)
+
+my_list["Suryanarayana","mahesh","ravi"]
+my_list.append["ramkumar"]
+print(my_list)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+# Original list
+names = ["Rohith", "Pavan", "Avinash"]
+
+#  Change an item
+names[1] = "Bhargav"
+
+#  Add a new item
+names.append("Saketh")
+
+#  Remove an item
+names.remove("Avinash")
+
+print(names)
+
+
+my_list=["mahesbabu","ntr","prabhas","ramcharan","alluarjun"]
+my_list.append("pavan kalyan")
+print(my_list)
+
+my_list=["paddy","wheat","maizecoto","watermelon"]
+my_list.pop(1)
+print(my_list)
+
+my_list=["mango","papaya","apple","carrot","beetroot","delhi","frog"]
+my_list.sort()
+print(my_list)
+
+
+
+my_list1=["man","women","Boy","girl","children"]
+my_list2=["stupid","concept","wrong"]
+my_list3=my_list1 + my_list2
+print(my_list3)
+
+
+names=["pavan","cherry","hero","lamp"]
+names.clear()
+print(names)
+
+heroes=["mahesh","ntr","prabhas","ram",]
+heroes.clear()
+print(heroes)
+
+family=["subrahmanyam","jyothi","Rohith","yeswanth","satyananarayna","kamala kumari"]
+family.copy
+print(family)
+
+village=["rangapuram","nagallavarigude","gokavaram","narsannpalem"]
+village.count(x)
+print(village)
+
+fruits=["banana","mango","pineapple"]
+new_fruits=["watermelon","papaya"]
+fruits.extend(new_fruits)
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits.index("banana"))
+
+
+fruits = ["apple", "banana"]
+fruits.insert(1, "cherry")  # Insert "cherry" at index 1
+print(fruits)
+
+thisdict = {
+ "name":"Rohith",
+ "college":"gitam",
+ "branch": "cse",
+}
+thisdict["gitam"]="klu"
+print(thisdict)
+
+thisdict = {
+    "colour":"white",
+    "model" :"2024",
+    "Brand":"scorpio-n",
+    "gear":"automatic",
+}
+thisdict["auto pilot"]="no" 
+print(thisdict)
+
+
+my_set={"rohith","bonthu","GITAM",1,"False"}
+print(my_set)
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+
+my_set ={"power","rangers","marvel"}
+my_set.add("dc marvel")
+print(my_set)
+
+my_set={"powerstar","superstar","stylishstar"}
+my_set.pop()
+print(my_set)
+
+my_set={"kingcobra","snakes","foreign"}
+my_set.clear()
+print(my_set)
+
+my_set={"gaytrhi","enterprises",2025,"all rights"}
+my_set.add("not resrved")
+print(my_set)
+
+my_list=["bigboss","host","Nagarjuna","season",8]
+my_list.reverse()
+print(my_list)
+
+my_list=[2028,"will","becomes","very","Intresting"]
+my_list.append("natural star nani")
+print(my_list)
+
+
+my_list=["python","is","very","intresting"]
+my_list.count
+print(my_list)
+
+my_list=["pyhton","is","not","intresting"]
+# my_list.index("intresting")
+print(my_list)
+
+my_set={"gaytrhi","enterprises",2025,"all rights"}
+my_set.remove("all rights")
+print(my_set)
+
+my_tuple=("name","king kong","cobra","snakes")
+my_tuple.count
+print(my_tuple) 
+
+my_list=["Bonthu","Bollina","Nama","Nagalla"]
+my_list.pop()
+print(my_list)
+ 
+my_tuple=("king","movies","is","Beautifull")
+print(my_tuple)
+
+thisdict={
+"college":"gitam",
+"Brnach":"cse",
+"college type":"University",
+"Branch":"cse"
+}
+thisdict["college"]="klu"
+print(thisdict)
+
+thisdict={
+"college":"gitam",
+"Brnach":"cse",
+"college type":"University",
+"Branch":"cse"
+}
+thisdict["college"]="klu"
+print(thisdict["college type"])
+
+this_set={"man","women","girl","Boy"}
+ this_set.add("another gender")
+ print(this_set) 
+
+this_set={"youngmna","elder","boys"}
+print(this_set)
+ 
+this_dict={
+    "brand":"mahindra",
+    "year of model":"twenty four",
+    "gear type":"automatic"
+}
+ this_dict["brand"]="toyato"
+ print(this_dict)
